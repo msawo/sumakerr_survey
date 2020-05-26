e24cd0c372b369f7ef9489a216251f8a9ff0acf4
msawo <msawo.inc@gmail.com>
2020-05-25 22:37:26 -0400

    Update color palettes

--- lib/common.dart
+++ lib/common.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 const white = Colors.white;
 const black = Colors.black;
+const grey = Color(0xff464e59);
 const red = Colors.red;
-const greenIsh = Color(0xff4dbc15);
+const greenIsh = Color(0xff1ec677);
 const blueIsh = Color(0xff36c5f0);
 const yellowIsh = Color(0xffecb22e);
 const greenAccent = Colors.greenAccent;
