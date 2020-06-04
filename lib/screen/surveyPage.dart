0137a1ba75ea78056231598250f3a979aebb4631
msawo <msawo.inc@gmail.com>
2020-06-03 22:26:21 -0400

    Pass totalScore to Reset widget

--- lib/screen/surveyPage.dart
+++ lib/screen/surveyPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/survey.dart';
-import '../widgets/surveyAnswer.dart';
 import '../widgets/resetSurvey.dart';
 import '../widgets/greeting.dart';
 import 'package:sumakerr_survey_app/common.dart';
@@ -15,6 +14,7 @@ class SurveyPage extends StatefulWidget {
 
 class _SurveyPageState extends State<SurveyPage> {
   int _questionIndex = 0;
+  int _totalScore = 0;
 
   String _welcomeText = 'Hello there!';
   String _welcomeMessage = 'Let\'s start with some basic info.\n';
@@ -23,87 +23,96 @@ class _SurveyPageState extends State<SurveyPage> {
     {
       'surveyQuestion': 'Please select a project location:',
       'userPreference': [
-        'Brusubi',
-        'Brufut',
-        'Kololi',
-        'Salagi',
-        'Sanyang',
+        {'selection': 'Brusubi', 'score': 10},
+        {'selection': 'Brufut', 'score': 9},
+        {'selection': 'Kololi', 'score': 9},
+        {'selection': 'Salagi', 'score': 8},
+        {'selection': 'Sanyang', 'score': 7},
+        {'selection': 'Tanji', 'score': 5},
       ],
     },
     {
       'surveyQuestion': 'What service(s) are you looking for?',
       'userPreference': [
-        'Design service',
-        'Construction service',
+        {'selection': 'Design service', 'score': 6},
+        {'selection': 'Construction service', 'score': 8},
       ],
     },
     {
       'surveyQuestion': 'How many rooms do you want in your new home?',
       'userPreference': [
-        'One to two',
-        'Three to four',
-        'Five to six',
-        'Seven or more',
+        {'selection': 'One to two', 'score': 4},
+        {'selection': 'Three to four', 'score': 6},
+        {'selection': 'Five to six', 'score': 8},
+        {'selection': 'Seven or more', 'score': 10},
       ],
     },
     {
       'surveyQuestion': 'How many bedrooms would you like in your new house?',
       'userPreference': [
-        'One',
-        'Two',
-        'Three',
-        'Four or more',
-        'I\'m not sure',
+        {'selection': 'One', 'score': 3},
+        {'selection': 'Two', 'score': 6},
+        {'selection': 'Three', 'score': 8},
+        {'selection': 'Four', 'score': 10},
+        {'selection': 'I\'m not sure', 'score': 0},
       ],
     },
-    {
-      'surveyQuestion': 'How many bathrooms would you like in your new house?',
-      'userPreference': [
-        'One',
-        'Two',
-        'Three',
-        'Four or more',
-        'I\'m not sure',
-      ],
-    },
-    {
-      'surveyQuestion':
-          'Have you already purchased the land where the home will be built?',
-      'userPreference': [
-        'I own a plot',
-        'Currently looking',
-        'Process of purchasing',
-      ],
-    },
-    {
-      'surveyQuestion': 'What is your current status of this project?',
-      'userPreference': [
-        'Exploring ideas',
-        'Planning/budgeting',
-        'Ready to hire',
-        'In progress',
-      ],
-    },
-    {
-      'surveyQuestion': 'When do you need to start the project?',
-      'userPreference': [
-        'Within a week',
-        'Within a month',
-        'Within six months',
-        'Not sure',
-      ],
-    },
-    {
-      'surveyQuestion': 'What\'s your budget?',
-      'userPreference': [
-        'D250,000',
-        'D500,000',
-        'D1 Million+',
-      ],
-    },
+    // TODO: Update the rest of the userSelections below
+    // {
+    //   'surveyQuestion': 'How many bathrooms would you like in your new house?',
+    //   'userPreference': [
+    //     {'selection': '', 'score': 6},
+    //     'One',
+    //     'Two',
+    //     'Three',
+    //     'Four or more',
+    //     'I\'m not sure',
+    //   ],
+    // },
+    // {
+    //   'surveyQuestion':
+    //       'Have you already purchased the land where the home will be built?',
+    //   'userPreference': [
+    //     {'selection': '', 'score': 6},
+    //     'I own a plot',
+    //     'Currently looking',
+    //     'Process of purchasing',
+    //   ],
+    // },
+    // {
+    //   'surveyQuestion': 'What is your current status of this project?',
+    //   'userPreference': [
+    //     {'selection': '', 'score': 6},
+    //     'Exploring ideas',
+    //     'Planning/budgeting',
+    //     'Ready to hire',
+    //     'In progress',
+    //   ],
+    // },
+    // {
+    //   'surveyQuestion': 'When do you need to start the project?',
+    //   'userPreference': [
+    //     {'selection': '', 'score': 6},
+    //     'Within a week',
+    //     'Within a month',
+    //     'Within six months',
+    //     'Not sure',
+    //   ],
+    // },
+    // {
+    //   'surveyQuestion': 'What\'s your budget?',
+    //   'userPreference': [
+    //     {'selection': '', 'score': 6},
+    //     'D250,000',
+    //     'D500,000',
+    //     'D1 Million+',
+    //   ],
+    // },
+
   ];
 
-  void _surveyResponse() {
+  void _surveyResponse(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex += 1;
     });
@@ -118,6 +127,7 @@ class _SurveyPageState extends State<SurveyPage> {
   void _resetSurvey() {
     setState(() {
       _questionIndex = 0;
+      _totalScore = 0;
     });
   }
 
@@ -149,7 +159,7 @@ class _SurveyPageState extends State<SurveyPage> {
                       surveyData: _surveyList,
                       surveyResponse: _surveyResponse,
                     )
-                  : ResetSurvey(resetHandler: _resetSurvey,),
+                  : ResetSurvey(resetHandler: _resetSurvey, resultScore: _totalScore,),
             ],
           ),
         ),
