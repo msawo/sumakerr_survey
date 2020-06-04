f812de9db85d63c135f85b3a31d5946b8d182d08
msawo <msawo.inc@gmail.com>
2020-06-03 22:28:42 -0400

    Add getter to display score result phrase

--- lib/widgets/resetSurvey.dart
+++ lib/widgets/resetSurvey.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:sumakerr_survey_app/common.dart';
 
 class ResetSurvey extends StatelessWidget {
+  final int resultScore;
   final Function resetHandler;
 
-  ResetSurvey({this.resetHandler});
+  ResetSurvey({this.resetHandler, this.resultScore});
+
+  // Create a getter
+  String get resultPhrase {
+    var resultText;
+
+    if (resultScore >= 30) {
+      resultText = 'Here are some recommended professionals in your area';
+    } else if(resultScore >= 20) {
+      resultText = 'You can reach a Sumakerr rep at +220 7702323 to help you find the right professionals';
+    } else if(resultScore >= 10) {
+      resultText = 'Have us call you at anytime to discuss some prospects';
+    } else {
+      resultText = 'Explore some ideas here...';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +29,16 @@ class ResetSurvey extends StatelessWidget {
       child: Center(
         child: Column(
           children: <Widget>[
-            Text(
-              'No more questions\n',
-              style: TextStyle(
-                fontSize: 22,
-                color: grey,
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic,
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text(
+                resultPhrase,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 22,
+                  color: grey,
+                  fontWeight: FontWeight.w300,
+                ),
               ),
             ),
             FlatButton(
